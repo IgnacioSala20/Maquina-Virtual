147adba9860e745c0162c7b86d2d5b010e8bda91
IgnacioSala20 <ignaciosala2004@gmail.com>
2024-06-10 23:07:32 -0300

    Comienzo1

--- MaquinaVirtual.py
+++ MaquinaVirtual.py
@@ -11,26 +11,29 @@
 #Esa funcion lo que hara es que tendra un valor que retornara que sera el valor que aumentara en la memoria para seguir leyendo los programas,
 #Luego lo que hara es depende el OP code,  sera ver los parametros y luego realizar lo que tenga que hacer esa operacion.
 
+#
+
 import sys
 
-#Definimos el Tamaño de la memoria
-memoria = [0] * 10
+# Definimos el Tamaño de la memoria
+# Inicializar la memoria con enteros en lugar de cadenas
+memoria = [0] * 65535
 
-def dump_mem(): #Almacenamos todo el contenido de memoria en un archivo llamado mem.dump
-    with open('mem.dump', 'wb') as d:
-        maux = [0, 0, 0, 160]
-        for m in maux:
-            d.write(bytes([m]))
-            
-#Abrimos el programa a ejecutar
+
+def dump_mem(memoria):
+    with open('mem.dump','wb') as d:
+        memoria_bytes=bytes(memoria) #Convertimos la variable en una cadena de bytes
+        d.write(memoria_bytes) #Lo escribimos en el archivo mem.dump
+
+# Abrimos el programa a ejecutar
 with open(sys.argv[1], 'rb') as programa:
     code = programa.read()
 
-#Recorremos y colocamos cada codigo en una posicion de memoria
+# Recorremos y colocamos cada codigo en una posicion de memoria
 pos = 0
 for c in code:
     if pos < 1025:
-        memoria[pos] = hex(c)
+        memoria[pos] = c
         pos += 1
 
-print(memoria)
+dump_mem(memoria)
