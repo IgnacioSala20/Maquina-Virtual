4bb197342ea3b986274d38dd5bf6c77aa9952981
IgnacioSala20 <ignaciosala2004@gmail.com>
2024-06-11 15:05:00 -0300

    Pruebas

--- MaquinaVirtual.py
+++ MaquinaVirtual.py
@@ -14,11 +14,41 @@
 #
 
 import sys
+def a0():
+    print("Hola Mundo")
+def a1():
+    pass
+def a2():
+    pass
+def a3():
+    pass
+def a4():
+    pass
+def a5():
+    pass
+def a6():
+    pass
+def a7():
+    pass
+funciones={
+    "a0":a0,
+    "a1":a0,
+    "a2":a0,
+    "a3":a0,
+    "a4":a0,
+    "a5":a0,
+    "a6":a0,
+    "a7":a0,
+}
 
+def extraerOp(valor):
+    print(hex(memoria[i])[2:len(hex(memoria[i]))])
+    return hex(memoria[i])[2:len(hex(memoria[i]))]
 # Definimos el Tamaño de la memoria
 # Inicializar la memoria con enteros en lugar de cadenas
-memoria = [0] * 65535
+memoria = [0] * 10
 
+operaciones=["a0","a1","a2","a3","a4","a5","a6","a7"]
 
 def dump_mem(memoria):
     with open('mem.dump','wb') as d:
@@ -35,5 +65,10 @@ for c in code:
     if pos < 1025:
         memoria[pos] = c
         pos += 1
+for i in range(len(memoria)):
+    a=extraerOp(memoria[i])
+    if a in operaciones:
+        funciones[a]()
+    
 
 dump_mem(memoria)
