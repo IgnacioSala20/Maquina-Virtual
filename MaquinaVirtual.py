e02f9518861b42f6d4c48ae8f4707fc901f96eba
IgnacioSala20 <ignaciosala2004@gmail.com>
2024-06-10 22:57:43 -0300

    Comienzo

--- MaquinaVirtual.py
+++ MaquinaVirtual.py
@@ -1,3 +1,16 @@
+#Es para ejecutar desde terminal
+#python3 MaquinaVirtual.py /home/ignacio/PruebasComandos/Maquina-Virtual/prog1.bin
+
+    #git add .
+    #git commit -m “Mensaje sobre el cambio que hicimos”
+    #git push origin master
+
+
+
+#El programa comenzara comparando la primer direccion con un VECTOR con todas las operaciones, si se encuentra la misma, lo que hacemos sera mandarlo a esa funcion,
+#Esa funcion lo que hara es que tendra un valor que retornara que sera el valor que aumentara en la memoria para seguir leyendo los programas,
+#Luego lo que hara es depende el OP code,  sera ver los parametros y luego realizar lo que tenga que hacer esa operacion.
+
 import sys
 
 #Definimos el Tamaño de la memoria
@@ -17,7 +30,7 @@ with open(sys.argv[1], 'rb') as programa:
 pos = 0
 for c in code:
     if pos < 1025:
-        memoria[pos] = c
+        memoria[pos] = hex(c)
         pos += 1
 
 print(memoria)
