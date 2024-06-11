7f21a8fa0918c558555cf9bba0bd1c53d7b4a446
IgnacioSala20 <ignaciosala2004@gmail.com>
2024-06-11 17:45:06 -0300

    Lauti

--- MaquinaVirtual.py
+++ MaquinaVirtual.py
@@ -7,9 +7,6 @@
 
 
 
-#El programa comenzara comparando la primer direccion con un VECTOR con todas las operaciones, si se encuentra la misma, lo que hacemos sera mandarlo a esa funcion,
-#Esa funcion lo que hara es que tendra un valor que retornara que sera el valor que aumentara en la memoria para seguir leyendo los programas,
-#Luego lo que hara es depende el OP code,  sera ver los parametros y luego realizar lo que tenga que hacer esa operacion.
 
 #
 
@@ -30,7 +27,7 @@ def a6():
     pass
 def a7():
     pass
-funciones={
+funciones={ #si "a0" esta en el vevtor operaciones va a hacer tal funcion que esta determinada aca:
     "a0":a0,
     "a1":a1,
     "a2":a2,
@@ -42,15 +39,18 @@ funciones={
 }
 
 def extraerOp(valor):
-    print(hex(memoria[i])[2:len(hex(memoria[i]))])
-    return hex(memoria[i])[2:len(hex(memoria[i]))]
+    print(hex(memoria[i])[2:len(hex(memoria[i]))]) #a0 d1 f4 10 ejemplo
+    return hex(memoria[i])[2:len(hex(memoria[i]))] #le saca la x al hexa
 
 def may255(valor): #Cuando usemos valores mayores a 255
     return bin(valor)[len(bin(valor))-8:len(bin(valor))]
 
 # Definimos el Tamaño de la memoria
 # Inicializar la memoria con enteros en lugar de cadenas
-memoria = [0] * 10
+memoria = [0] * 100 #vector memoria
+#El programa comenzara comparando la primer direccion con un VECTOR con todas las operaciones, si se encuentra la misma, lo que hacemos sera mandarlo a esa funcion,
+#Esa funcion lo que hara es que tendra un valor que retornara que sera el valor que aumentara en la memoria para seguir leyendo los programas,
+#Luego lo que hara es depende el OP code,  sera ver los parametros y luego realizar lo que tenga que hacer esa operacion.
 
 operaciones=["a0","a1","a2","a3","a4","a5","a6","a7"]
 
@@ -62,6 +62,8 @@ def dump_mem(memoria):
 # Abrimos el programa a ejecutar
 with open(sys.argv[1], 'rb') as programa:
     code = programa.read()
+    
+print(code)
 
 # Recorremos y colocamos cada codigo en una posicion de memoria
 pos = 0
@@ -69,13 +71,16 @@ for c in code:
     if pos < 1025:
         memoria[pos] = c
         pos += 1
-for i in range(len(memoria)):
-    a=extraerOp(memoria[i])
-    if a in operaciones:
+print(memoria)  
+
+for i in range(len(memoria)): #lee memoria
+    a=extraerOp(memoria[i]) #
+    if a in operaciones: #si esta en el vector operaciones (compara)
         funciones[a]()
     
 print()
-for i in range(len(memoria)):
-    print(memoria[i])
+
+#for i in range(len(memoria)):
+ #   print(memoria[i])
 
 dump_mem(memoria)
