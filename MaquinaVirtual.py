7e2b904fcfffb2c016f583fcefc829dfc5fd8ac5
IgnacioSala20 <ignaciosala2004@gmail.com>
2024-06-10 22:35:55 -0300

    PrimerPrueba

--- MaquinaVirtual.py
+++ MaquinaVirtual.py
@@ -1,7 +1,23 @@
-print("Hola Mundo")
-print()
-
 import sys
 
-with open('hola.txt', "r") as hola:
-    print()
+#Definimos el Tamaño de la memoria
+memoria = [0] * 10
+
+def dump_mem(): #Almacenamos todo el contenido de memoria en un archivo llamado mem.dump
+    with open('mem.dump', 'wb') as d:
+        maux = [0, 0, 0, 160]
+        for m in maux:
+            d.write(bytes([m]))
+            
+#Abrimos el programa a ejecutar
+with open(sys.argv[1], 'rb') as programa:
+    code = programa.read()
+
+#Recorremos y colocamos cada codigo en una posicion de memoria
+pos = 0
+for c in code:
+    if pos < 1025:
+        memoria[pos] = c
+        pos += 1
+
+print(memoria)
