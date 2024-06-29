b92c379f6e93a560199220e315ffbc9911d1be82
IgnacioSala20 <ignaciosala2004@gmail.com>
2024-06-29 10:49:48 -0300

    COmpleto

--- MaquinaVirtual.py
+++ MaquinaVirtual.py
@@ -1,32 +1,9 @@
-	
-#Es para ejecutar desde terminal
-#python3 MaquinaVirtual.py /home/ignacio/PruebasComandos/Maquina-Virtual/prog1.bin
-#python MaquinaVirtual.py /home/ignacio/PruebasComandos/Maquina-Virtual/prog1.bin
-
-
-
-
- #git add .
- #git commit -m “Mensaje sobre el cambio que hicimos”
- #git push origin master
- #xxd -s 53744 -l 128 /home/ignacio/PruebasComandos/Maquina-Virtual/mem.dump Para mostrar el volcado de memoria desde 53744 a la longitud 128
- #xxd /home/ignacio/PruebasComandos/Maquina-Virtual/mem.dump Muestra todo el mem.dump en linux
+#python MaquinaVirtual.py /home/ignacio/PruebasComandos/Maquina-Virtual/prog1.bin o python3 MaquinaVirtual.py /home/ignacio/PruebasComandos/Maquina-Virtual/prog1.bin
+#xxd -s 53744 -l 128 /home/ignacio/PruebasComandos/Maquina-Virtual/mem.dump Para mostrar el volcado de memoria desde 53744 a la longitud 128
 import sys
 xp=0
 memoria = [0] * 65535#vector memoria
 c=100
-
-#el operador 0xFF se utiliza para obtener los 8 bits menos significativos de un número
-def xor(cadena1,cadena2):
- return cadena1 ^ cadena2
-def negar(cadena):
-  var=str()
-  for i in range(len(cadena)):
-      if cadena[i]=="1":
-          var=var+"0"
-      elif cadena[i]=="0":
-          var=var+"1"
-  return var
 
 def a0():
    global c,memoria
@@ -52,6 +29,7 @@ def a2():
    else:
       print("Error")
       c = len(memoria)
+   
 def a3():
    global c,memoria
    var=(memoria[c+1] << 8) + memoria[c+2]
@@ -80,7 +58,7 @@ def a5():
       c=len(memoria)
    c=c+2
 
-def a6():
+def a6(): #el operador 0xFF se utiliza para obtener los 8 bits menos significativos de un número
    global xp, c, memoria
    var=(memoria[c+1] << 8) + memoria[c+2]
    memoria[var]=(xp>>8) & 0xFF # Obtener el byte alto 
@@ -96,8 +74,7 @@ def a7():
    xp=N1
    c=c+3
 
-
-def b0():
+def b0(): #Salto Condicional
    global c,memoria
    var=(memoria[c+1] << 8) + memoria[c+2]
    var2=(memoria[c+3] << 8) + memoria[c+4]
@@ -106,37 +83,35 @@ def b0():
    else:
       c=c+5
 
-
-def b1():
+def b1(): #Salto Directo
    global c,memoria
    var=(memoria[c+1] << 8) + memoria[c+2]
    c=var
 
-
-def c0():
+def c0(): #Necacion | el operador 0xFF se utiliza para obtener los 8 bits menos significativos de un número
    global c,memoria
    var1=(memoria[c+1] << 8) + memoria[c+2]
    #Usamos el ~ para negar(complemento a uno) un numero y & 0xFF para mantener los bits deseados
-   memoria[var1]=~(bin(memoria[var1])) &0xFF
+   memoria[var1]=~(bin(memoria[var1])) & 0xFF
    c=c+3
-def c1():
+   
+def c1(): #AND
    global c,memoria
    var1=(memoria[c+1] << 8) + memoria[c+2]
    var2=(memoria[c+3] << 8) + memoria[c+4]
-   conjuncion = memoria[var1] and memoria[var2]
+   conjuncion = memoria[var1] & memoria[var2] #Operador logico del and
    c=c+5
    memoria[var2]=conjuncion
    
-def c2():
+def c2(): #OR
    global c,memoria
    var1=(memoria[c+1] << 8) + memoria[c+2]
    var2=(memoria[c+3] << 8) + memoria[c+4]
-   disyuncion = memoria[var1] or memoria[var2]
+   disyuncion = memoria[var1] | memoria[var2] #Operador logico del or
    c=c+5
    memoria[var2]=disyuncion
 
-
-def c3():
+def c3(): #XOR | el operador 0xFF se utiliza para obtener los 8 bits menos significativos de un número
    global c,memoria
    var1=(memoria[c+1] << 8) + memoria[c+2]
    var2=(memoria[c+3] << 8) + memoria[c+4]
@@ -144,8 +119,7 @@ def c3():
    memoria[var2]=resultado
    c=c+5
 
-
-def d0(): #Suma
+def d0(): #Suma | el operador 0xFF se utiliza para obtener los 8 bits menos significativos de un número
    global c,memoria
    var1=(memoria[c+1] << 8) + memoria[c+2]
    var2=(memoria[c+3] << 8) + memoria[c+4]
@@ -154,8 +128,7 @@ def d0(): #Suma
    memoria[var2]=a
    c=c+5
 
-
-def d1(): #Resta
+def d1(): #Resta | el operador 0xFF se utiliza para obtener los 8 bits menos significativos de un número
    global c,memoria
    var1=(memoria[c+1] << 8) + memoria[c+2]
    var2=(memoria[c+3] << 8) + memoria[c+4]
@@ -164,8 +137,7 @@ def d1(): #Resta
    memoria[var2]=suma
    c=c+5
 
-
-def d2(): #Modulo
+def d2(): #Modulo | el operador 0xFF se utiliza para obtener los 8 bits menos significativos de un número
    global c,memoria
    var1=(memoria[c+1] << 8) + memoria[c+2]
    var2=(memoria[c+3] << 8) + memoria[c+4]
@@ -175,18 +147,15 @@ def d2(): #Modulo
    c=c+5
    return memoria
 
-
-def d3(): #Revisar
+def d3(): #Incremento XP
    global xp, c, memoria
    xp=xp+1
    c=c+1
 
-
-def d4(): #Revisar
+def d4(): #Decremento XP
    global xp, c, memoria
    xp=xp-1
    c=c+1
-
 
 def f0():
    global c,memoria
@@ -195,22 +164,14 @@ def f0():
       memoria_bytes=bytes(memoria) #Convertimos la variable en una cadena de bytes
       d.write(memoria_bytes) #Lo escribimos en el archivo mem.dump
 
-
 def f1():
    global c,memoria
    c=len(memoria)+1
 
-
 #si "a0" esta en el vevtor operaciones va a hacer tal funcion que esta determinada aca:
 funciones={"0xa0":a0, "0xa1":a1, "0xa2":a2, "0xa3":a3, "0xa4":a4, "0xa5":a5, "0xa6":a6, "0xa7":a7, "0xb0":b0, "0xb1":b1, "0xc0":c0, "0xc1":c1, "0xc2":c2, "0xc3":c3, "0xd0":d0, "0xd1":d1, "0xd2":d2, "0xd3":d3, "0xd4":d4, "0xf0":f0, "0xf1":f1,}
 
-
-def may255(valor): #Cuando usemos valores mayores a 255
-   return int(bin(valor)[len(bin(valor))-8:len(bin(valor))],2)
-
-
 operaciones=["a0","a1","a2","a3","a4","a5","a6","a7","b0","b1","c0","c1","c2","c3","d0","d1","d2","d3","d4","f0","f1"]
-
 
 # Abrimos el programa a ejecutar
 with open(sys.argv[1], 'rb') as programa:
@@ -220,14 +181,12 @@ print(code)
 pos = 100
 for i in code:
    if pos < 1025:
-         memoria[pos]=i
-         pos += 1
-
+      memoria[pos]=i
+      pos += 1
 
 while c < len(memoria):
    a = hex(memoria[c])
    if a in funciones: # compara con funciones
-         funciones[a]()
+      funciones[a]()
    else:
-         c += 1
-         
+      c += 1
