cd465220d536a3a976c8d81197848bb241a2edff
IgnacioSala20 <ignaciosala2004@gmail.com>
2024-07-04 18:02:25 -0300

    Finalizado

--- MaquinaVirtual.py
+++ MaquinaVirtual.py
@@ -2,11 +2,11 @@ import sys
 xp=0
 memoria = [0] * 65535
 c=100
-
 def a0():
    global c,memoria
    var=(memoria[c+1] << 8) + memoria[c+2]
    memoria[var]=memoria[c+3]
+   print(memoria[var])
    c=c+4
 def a1():
    global c,memoria
@@ -14,16 +14,15 @@ def a1():
    memoria[var]=(c>>8) & 0xFF
    memoria[var+1]=c & 0xFF 
    c=c+3
-
 def a2():
    global c,memoria
-   var=(memoria[c+1] << 8) + memoria[c+2]
-   N1 = (memoria[var] << 8) + memoria[var + 1]
-   if N1 > 100 and N1<1025:
-      c= N1
+   var=(memoria[c+1]<< 8)+memoria[c+2]
+   N1=(memoria[var]<< 8)+memoria[var+1]
+   if N1>100 and N1<1025:
+      c=N1
    else:
       print("Error")
-      c = len(memoria)
+      c=len(memoria)
 def a3():
    global c,memoria
    var=(memoria[c+1] << 8) + memoria[c+2]
@@ -31,7 +30,6 @@ def a3():
    memoria[var2]=memoria[var]
    memoria[var]=0
    c=c+5
-   
 def a4():
    global xp,c,memoria
    var=(memoria[c+1] << 8) + memoria[c+2]
@@ -77,7 +75,7 @@ def b1():
 def c0():
    global c,memoria
    var1=(memoria[c+1] << 8) + memoria[c+2]
-   memoria[var1]=~(bin(memoria[var1])) & 0xFF
+   memoria[var1]=~(memoria[var1]) & 0xFF
    c=c+3
 def c1(): 
    global c,memoria
@@ -112,16 +110,17 @@ def d1():
    global c,memoria
    var1=(memoria[c+1] << 8) + memoria[c+2]
    var2=(memoria[c+3] << 8) + memoria[c+4]
-   suma=memoria[var2]-memoria[var1]
+   suma=memoria[var1]-memoria[var2]
    suma=suma & 0xFF
    memoria[var2]=suma
+
    c=c+5
 def d2(): 
    global c,memoria
    var1=(memoria[c+1] << 8) + memoria[c+2]
    var2=(memoria[c+3] << 8) + memoria[c+4]
-   suma=memoria[var1]%memoria[var2]
-   suma=suma % 0xFF
+   suma=memoria[var2]%memoria[var1]
+   suma=suma & 0xFF
    memoria[var2]=suma
    c=c+5
    return memoria
@@ -142,19 +141,15 @@ def f0():
 def f1():
    global c,memoria
    c=len(memoria)+1
-
 funciones={"0xa0":a0, "0xa1":a1, "0xa2":a2, "0xa3":a3, "0xa4":a4, "0xa5":a5, "0xa6":a6, "0xa7":a7, "0xb0":b0, "0xb1":b1, "0xc0":c0, "0xc1":c1, "0xc2":c2, "0xc3":c3, "0xd0":d0, "0xd1":d1, "0xd2":d2, "0xd3":d3, "0xd4":d4, "0xf0":f0, "0xf1":f1,}
 operaciones=["a0","a1","a2","a3","a4","a5","a6","a7","b0","b1","c0","c1","c2","c3","d0","d1","d2","d3","d4","f0","f1"]
-
 with open(sys.argv[1], 'rb') as programa:
    code = programa.read()
-print(code)
 pos = 100
 for i in code:
    if pos<1025:
       memoria[pos]=i
       pos+= 1
-
 while c<len(memoria):
    a=hex(memoria[c])
    if a in funciones:
